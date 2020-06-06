33e60cb20d84b5019bb62f18f30f4d8eebb387c9
VTC <vtchris@hotmail.com>
2020-06-06 14:21:41 -0400

    Update mongoose for security

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
-  { useNewUrlParser: true },
-  { useUnifiedTopology: true }
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 // Start the API server
